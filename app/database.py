2da0c5b34a7e4bd30fe9cdc90981f6fc02b5eb6a
SmartSorter <k01012000ya@gmail.com>
2026-01-23 00:54:13 +0200

    configure for deploying

--- app/database.py
+++ app/database.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine 
 from sqlalchemy.orm import sessionmaker, DeclarativeBase 
 
-DATABASE_URL = (
-    "postgresql+psycopg2://"
-    "smartsort_user:smartsort_pass@localhost:5432/smartsort"
-)
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://smartsort_user:smartsort_pass@localhost:5432/smartsort")
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine=create_engine (DATABASE_URL, echo=True)
 
